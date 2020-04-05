d174fd3db18210190a8fcaf8a1dae92ea47b0423
caleb crome <caleb@crome.org>
2020-04-05 09:30:02 -0700

    Sun Apr  5 09:30:02 PDT 2020

---
+++ unemployment.py
@@ -0,0 +1,41 @@
+import threading
+import urllib.request
+import pandas as pd
+import io
+import os
+import time
+
+class UnemploymentDataException(Exception):
+    pass
+
+new_claims_url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id=ICSA'
+continuing_claims_url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?id=CCSA'
+
+update_lock = threading.Lock()
+def _get_df(url, local_fn, expiry_age = 60*60*24):
+    """Gets the url as a pandas dataframe.  Only retrieves new data once a day"""
+    do_update = True
+    if os.path.exists(local_fn):
+        mtime = os.path.getmtime(local_fn)
+        now = time.time()
+        age = now-mtime
+        if age <= expiry_age:
+            do_update = False
+    if do_update:
+        update_lock.acquire()
+        df = None
+        with urllib.request.urlopen(url) as response:
+            data = response.read().decode('UTF-8')
+            with open(local_fn, "w") as f:
+                f.write(data)
+        update_lock.release()
+    df = pd.read_csv(local_fn)
+    return df
+
+def get_unemployment():
+    """retruns 2 data frames unemployment data: new claims and continuing claims.  Can raise an UnemploymentDataException if the data isnt' available."""
+    new_claims = _get_df(new_claims_url, "new_claims.csv")
+    continuing_claims = _get_df(continuing_claims_url, "continuing_claims.csv")
+    return new_claims, continuing_claims
+
+print (get_unemployment())
