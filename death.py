7238423b6ead02c5b275bbe4901e615aaaa17489
caleb crome <caleb@crome.org>
2020-04-17 08:30:02 -0700

    Fri Apr 17 08:30:02 PDT 2020

---
+++ death.py
@@ -0,0 +1,25 @@
+
+causes_of_death = {
+    'Heart Disease': 647457,
+    'Cancer': 599108,
+    'Accidents': 169936,
+    'CLRD': 160201,
+    'Stroke': 146383,
+    'Alzheimer': 121404,
+    'Diabetes': 83564,
+    'Flu&Pneumonia': 55672,
+    'Kidney Disease': 50633,
+    'Suicide': 47173,
+    'All Other Causes': 731972,
+}
+
+def s(x):
+    if x == 'All Other Causes':
+        return 0
+    else:
+        return causes_of_death[x]
+
+def get_causes():
+    return [(x, causes_of_death[x]) for x in sorted(causes_of_death, key=s)[::-1]]
+
+print(get_causes())
